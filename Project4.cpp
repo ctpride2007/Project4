698b625136b3a8db54e89874ed9d9ae08553b27f
ctpride07 <ctpride07@gmail.com>
2025-11-12 22:01:28 -0500

    Add project files.

---
+++ Project4.cpp
@@ -0,0 +1,16 @@
+/*
+*Carter Pride
+* Professor Schaffner
+* Write a program that uses a loop to display Pattern A below, followed by another loop that displays Pattern B.
+Note – Pattern A will appear first in the output, followed by Pattern B.
+* 11/2025
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
